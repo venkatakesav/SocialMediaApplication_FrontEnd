6faa157a73d220fad732bd382aec6812801d15b1
kesav5678 <venkata.kesav@students.iiit.ac.in>
2023-02-06 13:01:39 +0530

    Authentication Finished

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ import {
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 import UserPlaces from './places/pages/UserPlaces';
+import UpdatePlace from './places/pages/UpdatePlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
+import Auth from './user/pages/Auth';
 
 const App = () => {
   return (
@@ -21,6 +23,8 @@ const App = () => {
         <Route path="/" exact element={<Users/>}></Route>
         <Route path="/:userId/places" exact element={<UserPlaces/>}></Route>
         <Route path="/places/new" exact element={<NewPlace/>}></Route>
+        <Route path="/places/:placeId" exact element={<UpdatePlace/>}></Route>
+        <Route path="/Auth" exact element={<Auth></Auth>}></Route>
       </Routes>
       </main>
     </Router>
---
+++ src/user/pages/Auth.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { useForm } from '../../shared/hooks/form-hook'
+import {useState} from "react"
+import Card from '../../shared/components/UIElements/Card'
+import Input from '../../shared/components/FormElements/Input'
+import Button from '../../shared/components/FormElements/Button'
+import { validate, VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/validators'
+import './Auth.css'
+
+function Auth() {
+    const [isLoginMode, setIsLoginMode] = useState(true)
+    const [formState, inputHandler, setFormData] = useForm({
+        email: {
+            value: '',
+            isValid: false
+        },
+        password: {
+            value: '',
+            isValid: false
+        }
+    }, false);
+
+    const authSubmitHandler = event => {
+        event.preventDefault();
+        console.log(formState.inputs)
+    }
+
+    const switchModeHandler = () => {
+        if(!isLoginMode){
+            setFormData({
+                ...formState.inputs,
+                name: undefined
+            }, formState.inputs.email.isValid && formState.inputs.password.isValid)
+        }else{
+            setFormData({
+                ...formState.inputs,
+                name: {
+                    value: '', 
+                    isValid: false
+                },
+            }, false)
+        }
+        
+        setIsLoginMode(prevMode => !prevMode)
+    }
+  
+    return (
+    <Card className="authentication">
+        <h2>Login Required</h2>
+        <hr></hr>
+        <form onSubmit={authSubmitHandler}>
+            {!isLoginMode && <Input element="input" id="name" type="text" label="Name" validators={[VALIDATOR_REQUIRE()]}
+            errorText="Don't You Have a Name ;)" onInput={inputHandler}></Input>}
+            <Input element = "input" id="email" type="email" label="email"
+            validators = {[VALIDATOR_EMAIL()]} errorText="Please enter a valid email address"
+            onInput={inputHandler} ></Input>
+            <Input element = "input" id="password" type="password" label="password"
+            validators = {[VALIDATOR_MINLENGTH(5)]} errorText="Please enter a valid Password (At least 5 Characters)"
+            onInput={inputHandler} ></Input>
+            <Button type="submit" disabled={!formState.isValid}>{isLoginMode ? 'LOGIN' : 'SIGNUP'}</Button>
+        </form>
+        <Button inverse onClick={switchModeHandler}>SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}</Button>
+    </Card>
+  )
+}
+export default Auth
