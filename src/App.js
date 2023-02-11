b6a85eafd7fcada897fc88a7df839eac1a257d43
kesav5678 <venkata.kesav@students.iiit.ac.in>
2023-02-11 16:15:46 +0530

    Not Working User Places

--- src/App.js
+++ src/App.js
@@ -18,17 +18,20 @@ import { AuthContext } from './shared/context/auth-context';
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userId, setUserId] = useState(false);
 
-  const login = useCallback(() => {
+  const login = useCallback((uid) => {
     setIsLoggedIn(true);
+    setUserId(uid)
   }, [])
 
   const logout = useCallback(() => {
     setIsLoggedIn(false);
+    setUserId(null)
   }, [])
 
   return (
-    <AuthContext.Provider value={{isLoggedIn: isLoggedIn, login: login, logout: logout}}>
+    <AuthContext.Provider value={{isLoggedIn: isLoggedIn, userId: userId, login: login, logout: logout}}>
     <Router>
       <MainNavigation/>
       <main>
--- src/shared/context/auth-context.jsx
+++ src/shared/context/auth-context.jsx
@@ -2,6 +2,7 @@ import { createContext } from "react";
 
 export const AuthContext = createContext({
     isLogged: false,
+    userId: null,
     login: () => {},
     logout: () => {},
 });
